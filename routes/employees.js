8a47f41d23440eb8dca8aea5731664ed548181a5
Ryan <ryanmcquilkin@gmail.com>
2020-12-07 15:09:29 -0800

    fixed missing module.exports

--- routes/employees.js
+++ routes/employees.js
@@ -9,7 +9,6 @@ router.get('/', async function (req, res, next) {
 	const results = await sqlQuery(conn, 'SELECT * FROM employees').catch(
 		console.log
 	);
-	console.log(results);
 	res.send(results);
 });
 
@@ -18,9 +17,10 @@ router.post('/', async function (req, res, next) {
 	const { firstName, lastName, address1 } = req.params;
 	const results = await sqlQuery(
 		conn,
-
 		`INSERT INTO table_name (first_name, last_name, address_1)
 		VALUES (${firstName}, ${lastName}, ${address1});`
 	).catch(console.log);
 	res.send(results);
 });
+
+module.exports = router;
